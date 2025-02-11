98e0306bdcec6b7a35f622153c6ee13db5c82d36
Mikhail <dubovikov94@gmail.com>
2025-02-12 01:32:00 +0300

    Initialize core presentation module with essential utils

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.javax.inject)
+    implementation(libs.lifecycle.viewmodel.ktx)
 }
